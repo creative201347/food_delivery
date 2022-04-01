7ac43fa787ff38c2bd8b218e13adef7d020b75bb
Nabin Dhami <creative201347@gmail.com>
2022-04-01 11:21:02 +0545

    folder structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { MdShoppingBasket } from "react-icons/md";
+
+import logo from "../assets/logo.png";
+import avatar from "../assets/avatar.png";
+
+const Header = () => {
+  return (
+    <header className="fixed z-50 w-screen  p-6 px-16">
+      {/* Desktop and tablet  */}
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src={logo} className="w-8 object-cover" alt={logo} />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </div>
+
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8">
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out">
+              Home
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out">
+              Menu
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out">
+              About
+            </li>
+            <li className="text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out">
+              Services
+            </li>
+          </ul>
+          <div className="relative flex items-center justify-center">
+            <MdShoppingBasket className="text-textColor text-2xl ml-8" />
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center ">
+              <p className="text-xs text-white font-semibold ">2</p>
+            </div>
+          </div>
+
+          <img
+            src={avatar}
+            className="w-10 min-w-[40px] h-10 min-h-[40px] shadow-2xl"
+            alt="user-profile"
+          />
+        </div>
+      </div>
+
+      {/* Mobile  */}
+      <div className="flex md:hidden w-full h-full "></div>
+    </header>
+  );
+};
+
+export default Header;
