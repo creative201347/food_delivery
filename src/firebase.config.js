30f98932320c4b28a33a54b97874ed109bc04331
Nabin Dhami <creative201347@gmail.com>
2022-04-05 18:10:06 +0545

    linux shifting

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPI_ID,
+  apiKey: "AIzaSyBK6eJXIOqfDlzwXNSKRirM_LRC1albyp0",
+  authDomain: "food-delivery-4462f.firebaseapp.com",
+  databaseURL: "https://food-delivery-4462f-default-rtdb.firebaseio.com",
+  projectId: "food-delivery-4462f",
+  storageBucket: "food-delivery-4462f.appspot.com",
+  messagingSenderId: "683461926958",
+  appId: "1:683461926958:web:4cc29f27b5623a550e7fbf",
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
